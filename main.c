9bb232810b4785ee3a66ac57e773d0fa08f076fe
caiowp <dontdoityet@murena.io>
2023-06-08 09:31:48 -0300

    Polynomial sum using linkedLists

---
+++ main.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : le2exercicio52.c
+ Author      : Caio Morais
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+#include "poly.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	polyFile("polinomio.txt");
+	return EXIT_SUCCESS;
+}
---
+++ poly.h
@@ -0,0 +1,10 @@
+/*
+ * poly.h
+ *
+ *  Created on: 7 de jun. de 2023
+ *      Author: t00l
+ */
+
+void polyFile(char* file);
+void show_error(const char *error, const char *str, const char *p);
+unsigned char skip_spaces(char *p, char **pp);
